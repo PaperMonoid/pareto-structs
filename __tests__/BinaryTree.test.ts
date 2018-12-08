4601132c891d44bf53c82e06adef596bb4ab3c55
Daniel Santiago Aguila Torres <tritiummonoid@gmail.com>
2018-12-08 11:06:30 -0800

    add more unit tests for BinaryTree remove

--- __tests__/BinaryTree.test.ts
+++ __tests__/BinaryTree.test.ts
@@ -57,3 +57,35 @@ test("Binary tree add 0, 1, 2 remove 1 test", function() {
   tree = tree.remove(1);
   expect(tree.toArray()).toEqual([0, 2]);
 });
+
+test("Binary tree add 2, 1, 0, -1, -2 remove 0 test", function() {
+  let tree = new BinaryTree<number>(comparator) as SortedCollection<number>;
+  tree = tree.add(2);
+  tree = tree.add(1);
+  tree = tree.add(0);
+  tree = tree.add(-1);
+  tree = tree.add(-2);
+  tree = tree.remove(0);
+  expect(tree.toArray()).toEqual([-2, -1, 1, 2]);
+});
+
+test("Binary tree add 2, 1, 0.5, 0, -1, -2, -1.5 remove 1, -1  test", function() {
+  let tree = new BinaryTree<number>(comparator) as SortedCollection<number>;
+  tree = tree.add(2);
+  tree = tree.add(1);
+  tree = tree.add(0.5);
+  tree = tree.add(0);
+  tree = tree.add(-1);
+  tree = tree.add(-2);
+  tree = tree.add(-1.5);
+  tree = tree.remove(1);
+  tree = tree.remove(-1);
+  expect(tree.toArray()).toEqual([-2, -1.5, 0, 0.5, 2]);
+});
+
+test("Binary tree add 0 remove 0 test", function() {
+  let tree = new BinaryTree<number>(comparator) as SortedCollection<number>;
+  tree = tree.add(0);
+  tree = tree.remove(0);
+  expect(tree.toArray()).toEqual([]);
+});
