31054efcdcfe75f4c6ebd93d884e568d3fc72e99
TritiumMonoid <tritiummonoid@gmail.com>
2018-12-20 23:07:15 -0800

    refactor BinarySearchTree to be immutable

--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -2,105 +2,109 @@ import SortedCollection from "./SortedCollection";
 import Comparator from "./Comparator";
 
 class Node<T> {
-  public value: T;
-  public left: Node<T>;
-  public right: Node<T>;
+  public readonly value: T;
+  public readonly left: Node<T>;
+  public readonly right: Node<T>;
 
   public constructor(value: T, left?: Node<T>, right?: Node<T>) {
     this.value = value;
     this.left = left;
     this.right = right;
   }
-
-  public copy(): Node<T> {
-    return new Node<T>(this.value, this.left, this.right);
-  }
 }
 
 class BinarySearchTree<T> implements SortedCollection<T> {
-  public comparator: Comparator<T>;
-  public root: Node<T>;
+  public readonly comparator: Comparator<T>;
+  public readonly root: Node<T>;
+  public readonly count: number;
 
-  constructor(comparator: Comparator<T>, root?: Node<T>) {
+  constructor(comparator: Comparator<T>, root?: Node<T>, count?: number) {
     this.comparator = comparator;
     this.root = root;
+    this.count = count;
   }
 
-  public copy(): BinarySearchTree<T> {
-    return new BinarySearchTree<T>(
-      this.comparator,
-      this.root && this.root.copy()
-    );
+  private addNode(newNode: Node<T>, node: Node<T>): Node<T> {
+    if (newNode == null) {
+      return node;
+    } else if (node == null) {
+      return newNode;
+    } else if (this.comparator(newNode.value, node.value) > 0) {
+      return new Node<T>(
+        node.value,
+        node.left,
+        this.addNode(newNode, node.right)
+      );
+    } else {
+      return new Node<T>(
+        node.value,
+        this.addNode(newNode, node.left),
+        node.right
+      );
+    }
+  }
+
+  private addValue(value: T, node: Node<T>): Node<T> {
+    if (node == null) {
+      return new Node<T>(value);
+    } else if (this.comparator(value, node.value) > 0) {
+      return new Node<T>(
+        node.value,
+        node.left,
+        this.addValue(value, node.right)
+      );
+    } else {
+      return new Node<T>(
+        node.value,
+        this.addValue(value, node.left),
+        node.right
+      );
+    }
   }
 
   public add(value: T): SortedCollection<T> {
-    const tree = this.copy();
-    if (tree.root == null) {
-      tree.root = new Node<T>(value);
-    } else {
-      let node = tree.root;
-      while (node != null) {
-        if (this.comparator(value, node.value) > 0) {
-          if (node.right == null) {
-            node.right = new Node<T>(value);
-            node = null;
-          } else {
-            node.right = node.right.copy();
-            node = node.right;
-          }
-        } else {
-          if (node.left == null) {
-            node.left = new Node<T>(value);
-            node = null;
-          } else {
-            node.left = node.left.copy();
-            node = node.left;
-          }
-        }
-      }
+    return new BinarySearchTree<T>(
+      this.comparator,
+      this.addValue(value, this.root),
+      this.count + 1
+    );
+  }
+
+  public removeNode(value: T, node: Node<T>): Node<T>[] {
+    if (node == null) {
+      return null;
     }
-    return tree;
+
+    const comparison = this.comparator(value, node.value);
+    if (comparison > 0) {
+      const removed = this.removeNode(value, node.right);
+      if (removed == null) {
+        return null;
+      } else {
+        return [removed[0], new Node<T>(node.value, node.left, removed[1])];
+      }
+    } else if (comparison < 0) {
+      const removed = this.removeNode(value, node.left);
+      if (removed == null) {
+        return null;
+      } else {
+        return [removed[0], new Node<T>(node.value, removed[1], node.right)];
+      }
+    } else {
+      return [node.left, node.right];
+    }
   }
 
   public remove(value: T): SortedCollection<T> {
-    if (this.root == null) {
+    const removed = this.removeNode(value, this.root);
+    if (removed == null) {
       return this;
     } else {
-      const tree = this.copy();
-      let parent = null;
-      let node = tree.root;
-      while (node != null) {
-        let comparison = this.comparator(value, node.value);
-        if (comparison > 0) {
-          node.right = node.right && node.right.copy();
-          parent = node;
-          node = node.right;
-        } else if (comparison < 0) {
-          node.left = node.left && node.left.copy();
-          parent = node;
-          node = node.left;
-        } else {
-          if (parent == null) {
-            const left = this.copy();
-            left.root = left.root.left;
-            const right = this.copy();
-            right.root = right.root.right;
-            return right.union(left);
-          } else if (parent.right == node) {
-            parent.right = node.right;
-            const left = this.copy();
-            left.root = node.left;
-            return tree.union(left);
-          } else if (parent.left == node) {
-            parent.left = node.right;
-            const left = this.copy();
-            left.root = node.left;
-            return tree.union(left);
-          }
-          node = null;
-        }
-      }
-      return this;
+      return new BinarySearchTree<T>(
+        this.comparator,
+        this.addNode(removed[0], removed[1]),
+        this.count - 1
+      );
     }
   }
 
@@ -200,9 +204,7 @@ class BinarySearchTree<T> implements SortedCollection<T> {
   }
 
   public size(): number {
-    return this.reduce<number>(0, function(accumulator, _) {
-      return accumulator + 1;
-    });
+    return this.count;
   }
 
   public toArray(): T[] {
