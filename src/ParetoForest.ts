4c497e27b1ae986ab4161ffba40f906538cac995
TritiumMonoid <tritiummonoid@gmail.com>
2019-02-20 22:31:37 -0800

    add SortedCollection to ParetoForest

--- src/ParetoForest.ts
+++ src/ParetoForest.ts
@@ -1,6 +1,9 @@
 import Frontier from "./frontier";
+import SortedCollection from "./sorted-collection";
 
 export default class ParetoForest<E> {
+  public readonly frontiers: SortedCollection<Frontier<E>>;
+
   public add(element: E): ParetoForest<E> {
     throw new ReferenceError("Not implemented");
   }
