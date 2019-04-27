fc5bef1a7b032250b3a289e4e721e0475bebd5a3
PaperMonoid <daniel.santiago.aguila.torres@gmail.com>
2019-04-27 00:28:25 -0700

    refactor: add other exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 export { ParetoStruct, Frontier } from "./pareto-struct";
+export { MultiMap, BinarySearchTree, RedBlackTree } from "./multimap";
+export { ListIterator } from "./list";
+export {
+  BiFunction,
+  Comparator,
+  Consumer,
+  Equality,
+  Equals,
+  Function,
+  Identity,
+  Predicate,
+  StrictEquality
+} from "./function";
+export { Optional } from "./data";
